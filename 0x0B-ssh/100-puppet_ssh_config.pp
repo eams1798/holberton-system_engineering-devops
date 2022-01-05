71c4c633444a5bfd0a1fc89e9190c13a82436316
estefanomisme <3401@holbertonschool.com>
2022-01-05 08:37:37 +0000

    puppet ssh config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,16 @@
 # sets up a client SSH configuration file so that we can connect to a server without using a password
-include stdlib
-file_line { 'Declare identity file':
-  path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/holberton',
-  replace => true,
+
+include ::ssh
+
+Ssh::Config_entry {
+  ensure => present,
+  path   => '~/.ssh/config'
 }
 
-file_line { 'Turn off passwd auth':
-  path    => '/etc/ssh/ssh_config',
-  line    => '    PasswordAuthentication no',
-  replace => true,
+ssh::config_entry { 'identityfile with no passwd':
+  host  => '*',
+  lines => [
+    '    PasswordAuthentication no',
+    '    IdentityFile ~/.ssh/school'
+  ]
 }
