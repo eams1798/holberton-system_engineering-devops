6c573008a3316dee7923e1d7079a9d845efb8672
estefanomisme <3401@holbertonschool.com>
2022-01-06 20:28:00 +0000

    wrong identityfile

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # sets up a client SSH configuration file so that we can connect to a server without using a password
 
 file_line { 'Declare identity file':
+  ensure  => 'present',
   path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/holberton',
-  replace => true,
+  line    => '    IdentityFile ~/.ssh/school'
 }
 
 file_line { 'Turn off passwd authentication':
+  ensure  => 'present',
   path    => '/etc/ssh/ssh_config',
-  line    => '    PasswordAuthentication no',
-  replace => true,
+  line    => '    PasswordAuthentication no'
 }
