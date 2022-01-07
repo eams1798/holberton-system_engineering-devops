0d023db207e6be334fbaa75e91a21b016d828a9d
estefanomisme <3401@holbertonschool.com>
2022-01-07 04:06:44 +0000

    first web server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,31 @@
+# Configure a NGINX server with Puppet
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'Add redirection, 301':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  ensure  => file,
+  path    => '/var/www/html/index.html',
+  mode    => '0644'
+  content => 'Hellow World'
+}
+
+file { '/usr/share/nginx/html/404.html':
+  ensure  => file,
+  path    => '/usr/share/nginx/html/404.html',
+  mode    => '0644'
+  content => 'Ceci n\'est pas une page'
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
