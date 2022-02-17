a8795ab526c9d72373206d6bf75dde0fb7cca11d
estefanomisme <3401@holbertonschool.com>
2022-02-16 23:59:43 -0500

    web stack debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,8 @@
+# fixing typing error at server configfile
+exec { 'replace':
+  command => "sudo sed -ie 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php"
+}
+
+exec { 'restart':
+  command => "sudo cat /var/www/html/wp-settings.php | grep class-wp-locale.php"
+}
